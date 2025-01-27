d35b1ffdde9c40311762b1107dc856f4c434a709
astra0081. <sneer-anyhow-jump@duck.com>
2025-01-27 19:24:33 +0530

    Triggering GitHub actions

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -135,7 +135,7 @@ void set_default_settings()
 	settings->setDefault("fastdig", "false");
 	settings->setDefault("dont_point_nodes", "false");
 //	settings->setDefault("dodge_rotation_speed", "0.08");
-	// Trigger
+	
 	
 
 	// Keymap
